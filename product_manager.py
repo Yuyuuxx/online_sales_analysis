02c8f672e91b6b63e887aa9c97d0adb458a67b7b
Yuyu <Igracmali26@outlook.com>
2025-03-12 19:28:59 +0100

    Added removal method in product_manager.py

--- product_manager.py
+++ product_manager.py
@@ -17,3 +17,12 @@ class ProductManager:
     def total_value(self):
         total_value = sum(product.price * product.quantity for product in self.products)
         return total_value
+    
+    def remove_product(self):
+        product_name = input("Enter the name of the product you want to remove.\n")
+        for product in self.products:
+            if product_name == product.name:
+                self.products.remove(product)
+                return f"Product {product_name} has been removed."
+        
+        return(f"Product {product_name} does not exist.")
