2a01fff4ec882087ad011c1ca875abaa6e00c3d9
Yuyu <Igracmali26@outlook.com>
2025-03-12 20:00:21 +0100

    Added cart functionality and modified main.py functionality

---
+++ cart.py
@@ -0,0 +1,29 @@
+from product import Product
+
+class Cart:
+    def __init__(self):
+        self.cart_items = []
+
+    def add_to_cart(self, product: Product, quantity: int):
+        
+        if quantity <= product.quantity:
+            self.cart_items.append({"product": product, "quantity": quantity})
+            product.add_quantity(-quantity)
+        else:
+            print(f"Not enough stock for {product.name}. Available: {product.quantity}")
+        
+    def calculate_total(self):
+            
+        total = sum(item["product"].price * item["quantity"] for item in self.cart_items)
+        return total
+    
+    def display_cart(self):
+
+        if not self.cart_items:
+            print("Your cart is empty.")
+        else:
+            print("Cart contents:")
+            for item in self.cart_items:
+                product = item["product"]
+                quantity = item["quantity"]
+                print(f"{product.name} - {quantity} pcs - ${product.price * quantity}")
