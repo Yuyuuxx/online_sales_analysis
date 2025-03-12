1b9c0d74a172d90a13f04a363360bb43626c088e
Yuyu <Igracmali26@outlook.com>
2025-03-12 20:27:39 +0100

    Slightly modified main.py

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ from product_manager import ProductManager
 def main():
     manager = ProductManager()
 
-    manager.add_product(Product("Camera", 1500.77, 10))
-    manager.add_product(Product("Microwave", 6000.55, 20))
-    manager.add_product(Product("Pencils", 50.99, 69))
-    manager.add_product(Product("Useless Contraption", 99999.99, 1))
-   
-    print("Available Products:")
-    manager.show_products()
-
-    total_value = manager.total_value()
-    print(f"\nTotal Inventory Value: ${total_value}")
+    manager.add_product(Product("Camera", 1500.77, 15))
+    manager.add_product(Product("Microwave", 6000.55, 25))
+    manager.add_product(Product("Pencils", 50.99, 74))
+    manager.add_product(Product("Useless Contraption", 99999.99, 2))
